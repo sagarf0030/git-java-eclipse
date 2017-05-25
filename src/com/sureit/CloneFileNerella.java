c7d4ea421fd3e41f83bdd23532f3df60c5a0695d
Sagar Nerella <sagar.nerellaa@gmail.com>
2017-05-25 17:41:28 +0530

    changed by sagarf0030 Change-Id: I3d960b015110ac5a6fa6d4be5c60d17c41535ae2 Signed-off-by: Sagar Nerella <sagar.f0030@gmail.com>

--- src/com/sureit/CloneFileNerella.java
+++ src/com/sureit/CloneFileNerella.java
@@ -3,7 +3,7 @@ package com.sureit;
 public class CloneFileNerella {
 
 	public static void main(String[] args) {
-	System.out.println("this is sysout added by sagarf0030");
+	System.out.println("this is sysout added by sagarf0030 to github  ");
 	}
 
 }
